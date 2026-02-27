e76e9ec6324f0cee6212e626240d6400b3f49717
Meharunn <meharunnisha.s@iott.co.in>
2026-02-27 21:41:15 +0530

    Added Dynamic port for deployment

--- server.js
+++ server.js
@@ -101,4 +101,8 @@ app.put("/update/:id", async (req, res) => {
   res.json(updated);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
